3ab279d4ef083094e2cf76eb4113763ce53bb661
Camus Ulloa Jonsson <camus@perxhealth.com>
2024-03-27 16:36:47 +1100

    remove cloudwatch ecs events from configuration and export log group arn

--- _outputs.tf
+++ _outputs.tf
@@ -1,3 +1,7 @@
 output "aws_cloudwatch_log_group_arn" {
   value = aws_cloudwatch_log_group.default.arn
 }
+
+output "aws_cloudwatch_ecs_events_log_group_arn" {
+  value = aws_cloudwatch_log_group.ecs_events.arn
+}
--- cloudwatch-ecs-event-logs.tf
+++ cloudwatch-ecs-event-logs.tf
@@ -4,8 +4,7 @@ resource "aws_cloudwatch_log_group" "ecs_events" {
   tags = {
     ExportToS3 = var.cloudwatch_logs_export
   }
-}
-
+} 
 
 resource "aws_cloudwatch_event_rule" "ecs_events" {
   name          = "capture-ecs-events-${var.cluster_name}-${var.name}"
@@ -25,26 +24,4 @@ EOF
 resource "aws_cloudwatch_event_target" "ecs_events" {
   rule = aws_cloudwatch_event_rule.ecs_events.name
   arn  = aws_cloudwatch_log_group.ecs_events.arn
-}
-
-data "aws_iam_policy_document" "ecs_events" {
-  statement {
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:PutLogEventsBatch",
-    ]
-
-    resources = ["${aws_cloudwatch_log_group.ecs_events.arn}:*"]
-
-    principals {
-      identifiers = ["events.amazonaws.com", "delivery.logs.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-resource "aws_cloudwatch_log_resource_policy" "ecs_events" {
-  policy_document = data.aws_iam_policy_document.ecs_events.json
-  policy_name     = "capture-ecs-events-${var.cluster_name}-${var.name}"
 }
